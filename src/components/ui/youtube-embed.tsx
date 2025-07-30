168596a45497e1e09266603a011a4f509ade3413
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-07-30 13:46:02 -0700

    Add skeleton to youtube videos

--- src/components/ui/youtube-embed.tsx
+++ src/components/ui/youtube-embed.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
+import { Skeleton } from "@mantine/core";
 import "./youtube-embed.css";
 
 export default function YoutubeEmbed({ videoId }: { videoId: string }) {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="youtube-embed">
-      <iframe
-        style={{ border: "none" }}
-        width="480"
-        height="270"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        title="YouTube video player"
-        allowFullScreen
-      ></iframe>
+      <Skeleton
+        className="youtube-skeleton"
+        width="100%"
+        height="100%"
+        visible={loading}
+      >
+        <iframe
+          style={{ border: "none" }}
+          width="480"
+          height="270"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          allowFullScreen
+          onLoad={() => setLoading(false)}
+        ></iframe>
+      </Skeleton>
     </div>
   );
 }
