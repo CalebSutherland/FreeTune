2521ba9dc6afd55bde31e71aff3e5807c44583db
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-08-21 14:47:55 -0700

    removing loading for auth context

--- client/src/contexts/user-auth-context.tsx
+++ client/src/contexts/user-auth-context.tsx
@@ -46,11 +46,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     loading,
   };
 
-  return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = (): AuthContextType => {
