323e165afaa9c76caedc7c9efc511be176fa95eb
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-08-22 16:07:03 -0700

    Fix analytics and speed insights

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,7 +1,7 @@
 import AppProvider from "./provider";
 import AppRouter from "./router";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./app.css";
 
 export default function App() {
